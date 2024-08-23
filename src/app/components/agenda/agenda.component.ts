75d7ac4283f58d4737dc8d9e717f447c784ba398
GonzalezLarry <controlalo@outlook.com>
2024-08-23 16:17:23 -0300

    repair calendar

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -24,6 +24,10 @@ export class AgendaComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.fetchTurnos();
+  }
+
+  fetchTurnos():void{
     this._apiService.getPacientes().subscribe({
       next:data=>{
         this.turnosList = data;
@@ -34,17 +38,27 @@ export class AgendaComponent implements OnInit {
       }
     })
   }
-
   /**
    * 
    * @param dia 
    * Al hacer clic en un dia del calendario va a filtrar por ese dia en particular y buscar los turnos en esa fecha
    */
-  onDateClick(dia: string): void {
+  /* onDateClick(dia: string): void {
     const fecha = this.formatFecha(dia);
     console.log(fecha);
     this.turnosFiltrados = this.turnosList.filter(turno => turno.fecha_cons === fecha);
     this.selectedDate = dia;
+  } */
+    onDateClick(event: Event): void {
+      console.log(event.target as HTMLInputElement);
+      const inputElement = event.target as HTMLInputElement;
+      const dia = inputElement.value;
+      
+      const fecha = this.formatFecha(dia); // Ajusta el formato según tus necesidades
+      console.log(fecha);
+      
+      this.turnosFiltrados = this.turnosList.filter(turno => turno.fecha_cons === fecha);
+      this.selectedDate = dia;
   }
 
   /**
@@ -52,15 +66,15 @@ export class AgendaComponent implements OnInit {
    * @param dia 
    * Va a formatear el dia escogido y agregarle el mm/yyyy  
    */
-  formatFecha(dia: string): string {
+  formatFecha(fecha: string): string {
     /* const diaStr = dia < 10 ? `0${dia}` : `${dia}`;
     return `${diaStr}/06/2024`; */
-    
+    const [year, month, day] = fecha.split('-');
       // Separar la fecha en día, mes y año
       //const [dia2, mes, año] = dia.split('/');
-    
+      return `${year}-${month}-${day}`;
       // Formatear y devolver la nueva fecha en formato yyyy-mm-dd
-      return `2024-06-${dia}`;
+      //return `2024-06-${dia}`;
     
   }
 
